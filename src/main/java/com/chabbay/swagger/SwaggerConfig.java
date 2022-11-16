f81dff90eca01f5554e581e3c4ddfff1b137fba6
ChabbaY <linus.englert@gmx.de>
2022-11-16 20:41:56 +0100

    added swagger ui api info

--- src/main/java/com/chabbay/swagger/SwaggerConfig.java
+++ src/main/java/com/chabbay/swagger/SwaggerConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +20,15 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket postsApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.any())
-                .build();
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SE-Reisebuchungen")
+                        .description("Reisebuchungs-Tool")
+                        .version("1.0.0")
+                        .contact(new Contact(
+                                "Linus Englert",
+                                "https://chabbay.de",
+                                "info@chabbay.de"))
+                        .build());
     }
 
     @Override
